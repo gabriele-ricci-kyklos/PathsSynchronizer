7c9c7a8dcd408e80738f2e2f17a5b60d9e59e542
gabriele-ricci-kyklos <gabriele.ricci@kyklos-group.com>
2021-07-16 18:37:07 +0200

    checksum table underlying dictionary made readonly

--- PathsSynchronizer.Core/Checksum/DirectoryChecksumTable.cs
+++ PathsSynchronizer.Core/Checksum/DirectoryChecksumTable.cs
@@ -2,6 +2,7 @@
 using PathsSynchronizer.Core.Support.Compression;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -9,32 +10,37 @@ namespace PathsSynchronizer.Core.Checksum
 {
     public class DirectoryChecksumTable
     {
-        private readonly IDictionary<string, ulong> _checksumTable;
+        private readonly IReadOnlyDictionary<string, ulong> _checksumTable;
         public string DirectoryPath { get; }
         public FileChecksumMode Mode { get; }
 
         public int Count => _checksumTable.Count;
 
-        public DirectoryChecksumTable(string directoryPath, FileChecksumMode mode, IDictionary<string, ulong> table)
+        private DirectoryChecksumTable(DirectoryChecksumTableData data)
         {
-            if (string.IsNullOrWhiteSpace(directoryPath))
+            if (string.IsNullOrWhiteSpace(data.DirectoryPath))
             {
-                throw new ArgumentNullException(nameof(directoryPath), "The provided path is null or blank");
+                throw new ArgumentNullException(nameof(data.DirectoryPath), "The provided path is null or blank");
             }
 
-            if (!Directory.Exists(directoryPath))
+            if (!Directory.Exists(data.DirectoryPath))
             {
-                throw new DirectoryNotFoundException($"The path {directoryPath} has not been found");
+                throw new DirectoryNotFoundException($"The path {data.DirectoryPath} has not been found");
             }
 
-            DirectoryPath = directoryPath;
-            Mode = mode;
-            _checksumTable = table ?? new Dictionary<string, ulong>();
+            DirectoryPath = data.DirectoryPath;
+            Mode = data.Mode;
+            _checksumTable = data.ChecksumTable ?? new ReadOnlyDictionary<string, ulong>(new Dictionary<string, ulong>());
+        }
+
+        public DirectoryChecksumTable(string directoryPath, FileChecksumMode mode, IDictionary<string, ulong> table)
+            : this(new DirectoryChecksumTableData(table, directoryPath, mode))
+        {
         }
 
         public async Task<byte[]> SerializeAsync()
         {
-            DirectoryChecksumTableData<ulong> toSerializeObj =
+            DirectoryChecksumTableData toSerializeObj =
                 new()
                 {
                     Mode = Mode,
@@ -49,8 +55,8 @@ namespace PathsSynchronizer.Core.Checksum
         public static async Task<DirectoryChecksumTable> FromSerializedAsync(byte[] bytes)
         {
             string json = await GZipHelper.DecompressStringAsync(bytes).ConfigureAwait(false);
-            DirectoryChecksumTableData<ulong> data = JsonConvert.DeserializeObject<DirectoryChecksumTableData<ulong>>(json);
-            return new DirectoryChecksumTable(data.DirectoryPath, data.Mode, data.ChecksumTable);
+            DirectoryChecksumTableData data = JsonConvert.DeserializeObject<DirectoryChecksumTableData>(json);
+            return new DirectoryChecksumTable(data);
         }
     }
 }
--- PathsSynchronizer.Core/Checksum/DirectoryChecksumTableData.cs
+++ PathsSynchronizer.Core/Checksum/DirectoryChecksumTableData.cs
@@ -1,11 +1,23 @@
 ﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace PathsSynchronizer.Core.Checksum
 {
-    public class DirectoryChecksumTableData<TChecksum>
+    public class DirectoryChecksumTableData
     {
-        public IDictionary<string, TChecksum> ChecksumTable { get; set; }
+        public IReadOnlyDictionary<string, ulong> ChecksumTable { get; set; }
         public string DirectoryPath { get; set; }
         public FileChecksumMode Mode { get; set; }
+
+        public DirectoryChecksumTableData()
+        {
+        }
+
+        public DirectoryChecksumTableData(IDictionary<string, ulong> checksumTable, string directoryPath, FileChecksumMode mode)
+        {
+            ChecksumTable = new ReadOnlyDictionary<string, ulong>(checksumTable ?? new Dictionary<string, ulong>());
+            DirectoryPath = directoryPath;
+            Mode = mode;
+        }
     }
 }
