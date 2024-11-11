c87a8180c1be40ee5c111814d88e1565fab8d746
Gabriele Ricci <gabriele.ricci@remira.com>
2024-11-11 13:54:01 +0100

    implemented XX hash provider with improved chunk hashing and external hdd management

---
+++ PathsSynchronizer.Core/Support/IO/WindowsExternalHDDHelper.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Management;
+
+namespace PathsSynchronizer.Core.Support.IO
+{
+    [System.Runtime.Versioning.SupportedOSPlatform("windows")]
+    internal static class WindowsExternalHDDHelper
+    {
+        private static readonly DriveInfo[] _externalDrives = GetExternalDrives();
+
+        internal static bool IsRemovableDrive(string path)
+        {
+            DriveInfo currentDrive = new(path);
+            return
+                _externalDrives
+                    .Select(x => x.Name)
+                    .Contains(currentDrive.Name);
+        }
+
+        private static DriveInfo[] GetExternalDrives()
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            List<DriveInfo> externalDrives = [];
+
+            ManagementObjectCollection allPhysicalDisks = new ManagementObjectSearcher("select MediaType, DeviceID from Win32_DiskDrive").Get();
+
+            foreach (ManagementBaseObject? physicalDisk in allPhysicalDisks)
+            {
+                ManagementObjectCollection allPartitionsOnPhysicalDisk = new ManagementObjectSearcher($"associators of {{Win32_DiskDrive.DeviceID='{physicalDisk["DeviceID"]}'}} where AssocClass = Win32_DiskDriveToDiskPartition").Get();
+                foreach (ManagementBaseObject? partition in allPartitionsOnPhysicalDisk)
+                {
+                    if (partition is null)
+                    {
+                        continue;
+                    }
+
+                    ManagementObjectCollection allLogicalDisksOnPartition = new ManagementObjectSearcher($"associators of {{Win32_DiskPartition.DeviceID='{partition["DeviceID"]}'}} where AssocClass = Win32_LogicalDiskToPartition").Get();
+                    foreach (ManagementBaseObject? logicalDisk in allLogicalDisksOnPartition)
+                    {
+                        if (logicalDisk is null)
+                        {
+                            continue;
+                        }
+
+                        DriveInfo? drive = drives.FirstOrDefault(x => x.Name.StartsWith(logicalDisk["Name"] as string ?? string.Empty, StringComparison.OrdinalIgnoreCase));
+                        if (drive is null)
+                        {
+                            continue;
+                        }
+
+                        string mediaType = physicalDisk["MediaType"] as string ?? string.Empty;
+                        if (mediaType.Contains("external", StringComparison.OrdinalIgnoreCase) || mediaType.Contains("removable", StringComparison.OrdinalIgnoreCase))
+                        {
+                            externalDrives.Add(drive);
+                        }
+                    }
+                }
+            }
+
+            return externalDrives.ToArray();
+        }
+    }
+}
--- PathsSynchronizer.Core/XXHash/XXHashProvider.cs
+++ PathsSynchronizer.Core/XXHash/XXHashProvider.cs
@@ -1,108 +1,55 @@
 ﻿using K4os.Hash.xxHash;
 using PathsSynchronizer.Core.Hashing;
-using System;
+using PathsSynchronizer.Core.Support.IO;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
-using System.Threading.Tasks.Dataflow;
 
 namespace PathsSynchronizer.Core.XXHash
 {
     public class XXHashProvider : IHashProvider<ulong>
     {
-        const int _chucksBufferSize = 1024 * 1024 * 50; //50MB
+        private static readonly bool _isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
 
-        public ValueTask<ulong> HashFileAsync(string filePath)
+        public async ValueTask<ulong> HashFileAsync(string filePath)
         {
-            FileInfo fileInfo = new(filePath);
-
-            Task<ulong> hashTask =
-                fileInfo.Length switch
-                {
-                    < _chucksBufferSize => HashFileOneShotAsync(filePath),
-                    _ => HashFileByChuncksWithProducerConsumerAsync(filePath)
-                };
-
-            return new ValueTask<ulong>(hashTask);
-        }
-
-        private static async ValueTask<ulong> HashFileByChuncksAsync(string filePath)
-        {
-            XXH64 fileHash = new();
-
-            using FileStream fs = File.OpenRead(filePath);
-            using BufferedStream bs = new(fs, _chucksBufferSize);
-
-            int bytesRead;
-            byte[] buffer = new byte[_chucksBufferSize];
-            while ((bytesRead = await bs.ReadAsync(buffer).ConfigureAwait(false)) > 0)
-            {
-                Memory<byte> memory = buffer.AsMemory(0, bytesRead);
-                fileHash.Update(memory.Span);
-            }
-
-            ulong chucksFileHash = fileHash.Digest();
-            return chucksFileHash;
-        }
-
-        #region Hash by chunks with producer/consumer
-        private static async Task<ulong> HashFileByChuncksWithProducerConsumerAsync(string filePath)
-        {
-            BufferBlock<Memory<byte>> dataFlowBuffer = new(new DataflowBlockOptions { EnsureOrdered = true });
-            Task<ulong> consumerTask = ConsumeAsync(dataFlowBuffer);
-            await ProduceFileChunks(filePath, dataFlowBuffer).ConfigureAwait(false);
-            ulong chucksFileHash = await consumerTask.ConfigureAwait(false);
-            return chucksFileHash;
-        }
-
-        private static async ValueTask ProduceFileChunks(string filePath, BufferBlock<Memory<byte>> dataFlowBuffer)
-        {
-            const int _chucksBufferSize = 1024 * 256; //0.25MB
-
-            using FileStream fs = File.OpenRead(filePath);
-            using BufferedStream bs = new(fs, _chucksBufferSize);
-
-            int bytesRead;
-            byte[] buffer = new byte[_chucksBufferSize];
-
-            while ((bytesRead = await bs.ReadAsync(buffer).ConfigureAwait(false)) > 0)
-            {
-                Memory<byte> memory = buffer.AsMemory(0, bytesRead);
-                dataFlowBuffer.Post(memory);
-            }
-
-            dataFlowBuffer.Complete();
-        }
-
-        private static async Task<ulong> ConsumeAsync(ISourceBlock<Memory<byte>> dataFlowBuffer)
-        {
-            XXH64 fileHash = new();
-            while (await dataFlowBuffer.OutputAvailableAsync().ConfigureAwait(false))
-            {
-                Memory<byte> memory = await dataFlowBuffer.ReceiveAsync().ConfigureAwait(false);
-                fileHash.Update(memory.Span);
-            }
-
-            ulong chucksFileHash = fileHash.Digest();
-            return chucksFileHash;
-        }
-
-        #endregion
-
-        private static async Task<ulong> HashFileOneShotAsync(string filePath)
-        {
-            byte[] allFile =
-                await File
-                    .ReadAllBytesAsync(filePath)
-                    .ConfigureAwait(false);
-
-            ulong oneShotFileHash = XXH64.DigestOf(allFile, 0, allFile.Length);
-            return oneShotFileHash;
+            bool isRemovableDrive = IsRemovableDrive(filePath);
+            int chucksBufferSize = isRemovableDrive ? 4096 : 1048576;
+            return await HashFileByChuncksAsync(filePath, chucksBufferSize).ConfigureAwait(false);
         }
 
         public ValueTask<ulong> HashBytesAsync(byte[] bytes)
         {
             ulong oneShotFileHash = XXH64.DigestOf(bytes, 0, bytes.Length);
             return ValueTask.FromResult(oneShotFileHash);
+        }
+
+        private static async ValueTask<ulong> HashFileByChuncksAsync(string filePath, int chucksBufferSize)
+        {
+            XXH64 fileHash = new();
+
+            using FileStream fs = File.OpenRead(filePath);
+
+            int bytesRead;
+            byte[] buffer = new byte[chucksBufferSize];
+            while ((bytesRead = await fs.ReadAsync(buffer).ConfigureAwait(false)) > 0)
+            {
+                fileHash.Update(buffer, 0, bytesRead);
+            }
+
+            ulong chucksFileHash = fileHash.Digest();
+            return chucksFileHash;
+        }
+
+        private static bool IsRemovableDrive(string path)
+        {
+            if (!_isWindows)
+            {
+                return false;
+            }
+
+#pragma warning disable CA1416
+            return WindowsExternalHDDHelper.IsRemovableDrive(path);
         }
     }
 }
