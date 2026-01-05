f0562f189c0806788c7eee051aeca7bcc6de1948
Gabriele Ricci <gabriele.ricci@remira.com>
2026-01-05 18:25:05 +0100

    added read and compare test

--- PathsSyncronizer.Test/MainTests.cs
+++ PathsSyncronizer.Test/MainTests.cs
@@ -37,7 +37,7 @@ namespace PathsSyncronizer.Test
             HashProgress progressDetails = default;
             Progress<HashProgress> progress = new(p => progressDetails = p);
 
-            DirectoryHash result = await service.ScanDirectoryAndHashAsync(@"C:\Temp\pictures\missing", progress);
+            DirectoryHash result = await service.ScanDirectoryAndHashAsync(@"C:\Development\dotnet\Kering-APEEvo", progress);
             result.Files.Should().HaveCountGreaterThan(0);
         }
 
@@ -72,6 +72,24 @@ namespace PathsSyncronizer.Test
             HashService service = new(ServiceOptions.Default, new XXHashProvider());
             DirectoryHash result = await service.ScanDirectoryAndHashAsync(@"E:\Foto");
             await StorageService.StoreDirectoryHashAsync(result, filePath);
+        }
+
+        [Fact]
+        public static async Task TestReadDirectoryScansAndCompare()
+        {
+            DirectoryHash eResult = await StorageService.ReadStorageFileAsync(@"C:\Temp\directoryhash_E_20260105161648.dat");
+            DirectoryHash fResult = await StorageService.ReadStorageFileAsync(@"C:\Temp\directoryhash_F_20260105163800.dat");
+
+            FileHash eFile = eResult.Files.Where(x => x.FilePath.Contains("IMG_1849.JPG")).First();
+            var fFile = fResult.Files.Where(x => x.FilePath.Contains("IMG_1849.JPG")).First();
+
+            var missingFiles = eResult.Files.Except(fResult.Files).ToArray();
+            var missingFiles2 = fResult.Files.Except(eResult.Files).ToArray();
+
+            var missingLines = missingFiles.Select(x => x.FilePath).Distinct().ToArray();
+            File.WriteAllLines("missing.txt", missingLines);
+
+
         }
     }
 }
